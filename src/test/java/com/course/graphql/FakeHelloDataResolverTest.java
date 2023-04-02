7b07c0e7884970e2ac8977f1164543f93f1b6e7b
Simon Vargas <sjvargas01@gmail.com>
2023-04-02 13:19:45 -0400

    created test cases

---
+++ src/test/java/com/course/graphql/FakeHelloDataResolverTest.java
@@ -0,0 +1,61 @@
+package com.course.graphql;
+
+import com.netflix.graphql.dgs.DgsQueryExecutor;
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static graphql.Assert.assertFalse;
+import static graphql.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class FakeHelloDataResolverTest {
+
+    @Autowired
+    DgsQueryExecutor dgsQueryExecutor;
+
+    @Test
+    void testOneHello() {
+        @Language("GraphQL") var graphqlQuery = """
+                {
+                oneHello {
+                text
+                randomNumber
+                    }
+                }
+                """;
+
+        String text = dgsQueryExecutor.executeAndExtractJsonPath(graphqlQuery, "data.oneHello.text");
+        Integer randomNumber = dgsQueryExecutor.executeAndExtractJsonPath(graphqlQuery, "data.oneHello.randomNumber");
+
+        assertFalse(StringUtils.isBlank(text));
+        assertNotNull(randomNumber);
+    }
+
+    @Test
+    void testAllHellos() {
+        @Language("GraphQL") var graphqlQuery = """
+                {
+                allHellos {
+                text
+                randomNumber
+                    }
+                }
+                """;
+
+        List<String> texts = dgsQueryExecutor.executeAndExtractJsonPath(graphqlQuery, "data.allHellos[*].text");
+        List<Integer> randomNumbers = dgsQueryExecutor.executeAndExtractJsonPath(graphqlQuery, "data.allHellos[*].randomNumber");
+
+        assertNotNull(texts);
+        assertFalse(texts.isEmpty());
+        assertNotNull(randomNumbers);
+        assertEquals(texts.size(), randomNumbers.size());
+
+
+    }
+}
